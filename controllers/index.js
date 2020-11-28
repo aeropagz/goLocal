0d5191ea80781da93995a0d216055243e8468d3e
Klaas <klaas.pelzer@gmx.de>
2020-11-28 11:18:37 +0100

    added routes for login and register (customer and farmers)

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,10 @@
-exports.homepage = function (req, res, next) {
+exports.homepage = function(req, res, next) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+}
+
+exports.showLogin = function(req, res, next) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+}
+exports.showRegister = function(req, res, next) {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,23 @@ let router = express.Router();
 
 let index = require("../controllers/index")
 
+
+//Login and Register
 router.get("/", index.homepage);
+router.get("/login/customer", index.showCustLogin);
+router.get("/register/customer", index.showCustRegister);
+router.post("/login/customer", index.showCustLogin);
+router.post("/register/customer", index.showCustRegister);
+router.get("/login/farmer", index.showCustLogin);
+router.get("/register/farmer", index.showCustRegister);
+router.post("/login/farmer", index.showCustLogin);
+router.post("/register/farmer", index.showCustRegister);
+
+//Endpoints for Dummy API for license
+
+//Endpoints for Product API
+
+
+
 
 module.exports = router;
