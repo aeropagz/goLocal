319b78c5def2afccfaa2566c0ad67ca6455ee596
AnkitGupta72 <ankitguptahs72@gmail.com>
2020-11-28 16:38:54 +0100

    adding name check for license validation

--- controllers/farmer.js
+++ controllers/farmer.js
@@ -4,7 +4,7 @@ validateLicense = async function (req, res, next) {
     if (!req.body || !req.body.data || !req.body.data.key || !req.body.data.name) {
         return res.status(400).send("Proper data should be provided.")
     }
-    result = await db.validateLicense(req.body.data.key);
+    result = await db.validateLicense(req.body.data.name, req.body.data.key);
     return res.json(result);
 }
 
