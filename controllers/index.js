8cf9fb0252cc847cc0d7aadf81e776786dcf98ad
Klaas <klaas.pelzer@gmx.de>
2020-11-28 11:24:31 +0100

    new controller file for user auth and register

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,3 @@
 exports.homepage = function(req, res, next) {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 }
-
-exports.showLogin = function(req, res, next) {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-}
-exports.showRegister = function(req, res, next) {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-}
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 let express = require("express");
 let router = express.Router();
 
-let index = require("../controllers/index")
+let index = require("../controllers/index");
+let user = require("../controllers/user");
 
-
-//Login and Register
+//Homepage Routes
 router.get("/", index.homepage);
-router.get("/login/customer", index.showCustLogin);
-router.get("/register/customer", index.showCustRegister);
-router.post("/login/customer", index.showCustLogin);
-router.post("/register/customer", index.showCustRegister);
-router.get("/login/farmer", index.showCustLogin);
-router.get("/register/farmer", index.showCustRegister);
-router.post("/login/farmer", index.showCustLogin);
-router.post("/register/farmer", index.showCustRegister);
+
+
+//Login and Register Endpoints
+router.get("/login/customer", user.showCustLogin);
+router.get("/register/customer", user.showCustRegister);
+router.post("/login/customer", user.showCustLogin);
+router.post("/register/customer", user.showCustRegister);
+router.get("/login/farmer", user.showCustLogin);
+router.get("/register/farmer", user.showCustRegister);
+router.post("/login/farmer", user.showCustLogin);
+router.post("/register/farmer", user.showCustRegister);
 
 //Endpoints for Dummy API for license
 
