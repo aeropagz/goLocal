11871c3990768270cc3570ceb9f7f1a2c07dc74c
Klaas <klaas.pelzer@gmx.de>
2020-12-06 16:40:01 +0100

    authGuard added, Login Component startet

--- frontend/goLocal/src/app/app.module.ts
+++ frontend/goLocal/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CookieModule } from "ngx-cookie";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CookieModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
