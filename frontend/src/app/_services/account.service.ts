c7b5692369c133153e5596179d597a371b3dbb2f
Klaas <klaas.pelzer@gmx.de>
2020-12-27 19:46:58 +0100

    logout function added

--- frontend/src/app/_services/account.service.ts
+++ frontend/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { Observable, BehaviorSubject } from "rxjs";
+import { Router } from "@angular/router";
 
 import { User } from "../_models/User";
 
@@ -12,7 +13,8 @@ export class AccountService {
   private userSubject: BehaviorSubject<User>;
   public user: Observable<User>;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient,
+              private router: Router) { 
     this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
     this.user = this.userSubject.asObservable();
   }
@@ -29,9 +31,17 @@ export class AccountService {
         return user;
       }));
   }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['/login']);
+  }
+
   registerUser(user: User){
     return this.http.post('http://localhost:8080/user/register/customer', user);
   }
+
   registerFarmer(user: User){
     return this.http.post('http://localhost:8080/user/register/farmer', user);
   }
